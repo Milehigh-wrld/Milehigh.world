c7ba951b54ea4781813ce227a218f800ffaca1bf
Cirrus.logic <167463404+lucentlabz@users.noreply.github.com>
2024-07-24 16:28:39 -0700

    Create OpenAI.py
    Signed-off-by: Cirrus.logic  <167463404+lucentlabz@users.noreply.github.com>

---
+++ OpenAI.py
@@ -0,0 +1,186 @@
+from openai import OpenAI
+client = OpenAI()
+
+client.batches.create(
+  input_file_id="file-abc123",
+  endpoint="/v1/chat/completions",
+  completion_window="24h"
+)
+
+{
+  "id": "batch_abc123",
+  "object": "batch",
+  "endpoint": "/v1/chat/completions",
+  "errors": null,
+  "input_file_id": "file-abc123",
+  "completion_window": "24h",
+  "status": "validating",
+  "output_file_id": null,
+  "error_file_id": null,
+  "created_at": 1711471533,
+  "in_progress_at": null,
+  "expires_at": null,
+  "finalizing_at": null,
+  "completed_at": null,
+  "failed_at": null,
+  "expired_at": null,
+  "cancelling_at": null,
+  "cancelled_at": null,
+  "request_counts": {
+    "total": 0,
+    "completed": 0,
+    "failed": 0
+  },
+  "metadata": {
+    "customer_id": "user_123456789",
+    "batch_description": "Nightly eval job",
+  }
+}
+GET
+ 
+https://api.openai.com/v1/batches/{batch_id}
+
+from openai import OpenAI
+client = OpenAI()
+
+client.batches.retrieve("batch_abc123")
+
+from openai import OpenAI
+client = OpenAI()
+
+client.batches.retrieve("batch_abc123")
+
+{
+  "id": "batch_abc123",
+  "object": "batch",
+  "endpoint": "/v1/completions",
+  "errors": null,
+  "input_file_id": "file-abc123",
+  "completion_window": "24h",
+  "status": "completed",
+  "output_file_id": "file-cvaTdG",
+  "error_file_id": "file-HOWS94",
+  "created_at": 1711471533,
+  "in_progress_at": 1711471538,
+  "expires_at": 1711557933,
+  "finalizing_at": 1711493133,
+  "completed_at": 1711493163,
+  "failed_at": null,
+  "expired_at": null,
+  "cancelling_at": null,
+  "cancelled_at": null,
+  "request_counts": {
+    "total": 100,
+    "completed": 95,
+    "failed": 5
+  },
+  "metadata": {
+    "customer_id": "user_123456789",
+    "batch_description": "Nightly eval job",
+  }
+}from openai import OpenAI
+client = OpenAI()
+
+client.batches.cancel("batch_abc123")
+
+{
+  "id": "batch_abc123",
+  "object": "batch",
+  "endpoint": "/v1/chat/completions",
+  "errors": null,
+  "input_file_id": "file-abc123",
+  "completion_window": "24h",
+  "status": "cancelling",
+  "output_file_id": null,
+  "error_file_id": null,
+  "created_at": 1711471533,
+  "in_progress_at": 1711471538,
+  "expires_at": 1711557933,
+  "finalizing_at": null,
+  "completed_at": null,
+  "failed_at": null,
+  "expired_at": null,
+  "cancelling_at": 1711475133,
+  "cancelled_at": null,
+  "request_counts": {
+    "total": 100,
+    "completed": 23,
+    "failed": 1
+  },
+  "metadata": {
+    "customer_id": "user_123456789",
+    "batch_description": "Nightly eval job",
+  }
+}
+from openai import OpenAI
+client = OpenAI()
+
+client.batches.list()
+{
+  "object": "list",
+  "data": [
+    {
+      "id": "batch_abc123",
+      "object": "batch",
+      "endpoint": "/v1/chat/completions",
+      "errors": null,
+      "input_file_id": "file-abc123",
+      "completion_window": "24h",
+      "status": "completed",
+      "output_file_id": "file-cvaTdG",
+      "error_file_id": "file-HOWS94",
+      "created_at": 1711471533,
+      "in_progress_at": 1711471538,
+      "expires_at": 1711557933,
+      "finalizing_at": 1711493133,
+      "completed_at": 1711493163,
+      "failed_at": null,
+      "expired_at": null,
+      "cancelling_at": null,
+      "cancelled_at": null,
+      "request_counts": {
+        "total": 100,
+        "completed": 95,
+        "failed": 5
+      },
+      "metadata": {
+        "customer_id": "user_123456789",
+        "batch_description": "Nightly job",
+      }
+    },
+    { ... },
+  ],
+  "first_id": "batch_abc123",
+  "last_id": "batch_abc456",
+  "has_more": true
+}
+
+{
+  "id": "batch_abc123",
+  "object": "batch",
+  "endpoint": "/v1/completions",
+  "errors": null,
+  "input_file_id": "file-abc123",
+  "completion_window": "24h",
+  "status": "completed",
+  "output_file_id": "file-cvaTdG",
+  "error_file_id": "file-HOWS94",
+  "created_at": 1711471533,
+  "in_progress_at": 1711471538,
+  "expires_at": 1711557933,
+  "finalizing_at": 1711493133,
+  "completed_at": 1711493163,
+  "failed_at": null,
+  "expired_at": null,
+  "cancelling_at": null,
+  "cancelled_at": null,
+  "request_counts": {
+    "total": 100,
+    "completed": 95,
+    "failed": 5
+  },
+  "metadata": {
+    "customer_id": "user_123456789",
+    "batch_description": "Nightly eval job",
+  }
+}
